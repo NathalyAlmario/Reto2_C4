f08ac983ef867e32342e5e720801bde8151d44a6
Nathaly Almario <almarionathaly.gmail.com>
2021-12-16 21:25:35 -0500

    deleteAll tables from main

--- src/main/java/Reto2_C4/Reto2C4Application.java
+++ src/main/java/Reto2_C4/Reto2C4Application.java
@@ -1,13 +1,28 @@
 package Reto2_C4;
 
+import Reto2_C4.interfaces.InterfaceCleaningProduct;
+import Reto2_C4.interfaces.InterfaceUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Reto2C4Application {
+public class Reto2C4Application implements CommandLineRunner {
 
+        @Autowired
+        private InterfaceCleaningProduct interfaceCleaningProduct;
+        @Autowired
+        private InterfaceUser interfaceUser;
+    
 	public static void main(String[] args) {
 		SpringApplication.run(Reto2C4Application.class, args);
 	}
+        
+        @Override
+        public void run(String ... args) throws Exception{
+        interfaceCleaningProduct.deleteAll();
+        interfaceUser.deleteAll();
+        }
 
 }
